7093e0e104fbc72f5ff9704b5f86b2233f973513
Wiktor Tarsa <wiktor.tarsa@gmail.com>
2021-05-16 18:36:33 +0200

    refactor sudoku.py

--- app/sudoku.py
+++ app/sudoku.py
@@ -50,19 +50,6 @@ class Sudoku:
                 self.solved_sudoku_numbers[pos_x][pos_y] = None
         return False
 
-    def check_rows_columns(self):
-        for i in range(9):
-            row = self.sudoku_numbers[i]
-            column = [row[i] for row in self.sudoku_numbers]
-            row_values = list(filter(lambda x: x in self.possible_values, row))
-            col_values = list(filter(lambda x: x in self.possible_values, column))
-            # Tricky one: we check if there are some duplicates:
-            check_row = (len(row_values) == len(set(row_values)))
-            check_col = (len(col_values) == len(set(col_values)))
-            if not check_row or not check_col:
-                return False
-        return True
-
     def check_squares(self):
         # We iterate through possible square indexes:
         for sq_x in range(0, 9, 3):
@@ -73,17 +60,6 @@ class Sudoku:
                 if len(square_values) != len(set(square_values)):
                     return False
         return True
-
-    def check_sudoku(self):
-        if (not self.check_rows_columns()) or (not self.check_squares()):
-            return "wrong"
-        valid_values = [[self.sudoku_numbers[x][y] in self.possible_values for x in range(9)] for y in range(9)]
-        count_valid_values = 0
-        for i in range(9):
-            count_valid_values += sum(valid_values[i])
-        if count_valid_values == 81:
-            return "all"
-        return "ok"
 
     def get_hint(self):
         self.solved_sudoku_numbers = deepcopy(self.sudoku_numbers)
@@ -96,39 +72,3 @@ class Sudoku:
             self.sudoku_numbers[i][j] = self.solved_sudoku_numbers[i][j]
             return i, j, self.solved_sudoku_numbers[i][j]
         return None
-
-    def generate_sudoku(self, mask_rate=0.6):
-        while True:
-            # Start with empty sudoku:
-            sudoku = np.zeros((9, 9), np.int)
-            possible_values_numpy = np.arange(1, 9 + 1)
-            # First row is a random permutation of [1,2,3,4,5,6,7,8,9]
-            sudoku[0, :] = np.random.choice(possible_values_numpy, 9, replace=False)
-            try:
-                for row in range(1, 9):
-                    for column in range(9):
-                        possible_column_vals = np.setdiff1d(possible_values_numpy, sudoku[:row, column])
-                        possible_row_vals = np.setdiff1d(possible_values_numpy, sudoku[row, :column])
-                        possible_vals = np.intersect1d(possible_column_vals, possible_row_vals)
-                        square_r, square_c = row // 3, column // 3
-                        possible_square_vals = np.setdiff1d(np.arange(0, 9 + 1),
-                                                            sudoku[square_r * 3:(square_r + 1) * 3,
-                                                            square_c * 3:(square_c + 1) * 3].ravel())
-                        possible = np.intersect1d(possible_vals, possible_square_vals)
-                        sudoku[row, column] = np.random.choice(possible, size=1)
-                break
-            except ValueError:
-                pass
-        mask_sudoku = sudoku.copy()
-        mask_sudoku[np.random.choice([True, False], size=sudoku.shape, p=[mask_rate, 1 - mask_rate])] = 0
-        self.sudoku_numbers = mask_sudoku.tolist()
-        self.changeable_numbers = [[mask_sudoku[y][x] not in self.possible_values for x in range(9)] for y in
-                                   range(9)]
-
-    def reset_sudoku(self):
-        self.sudoku_numbers = deepcopy(
-            [[self.sudoku_numbers[y][x] if not self.changeable_numbers[y][x] else None for x in range(9)] for y in
-             range(9)])
-
-    def change_value(self, x, y, value):
-        self.sudoku_numbers[x][y] = value
